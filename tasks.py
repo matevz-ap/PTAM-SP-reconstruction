3f84d6fa28e884d47d9fa6a9bcc6536d9052f578
matevz-ap <matepeklaj@gmail.com>
2023-06-29 23:01:18 +0200

    Add download recon task

--- tasks.py
+++ tasks.py
@@ -1,8 +1,26 @@
 import json
 import subprocess
 import re
+import requests
+import zipfile
 
-def make_response(uuid, numbers, success):
+server_url = "http://localhost:5000"
+
+def download_files(uuid):
+    response = requests.get(f"{server_url}/{uuid}/download")
+
+    if response.status_code == 200:
+        with open(f"./data/{uuid}.zip", "wb") as f:
+            f.write(response.content)
+        print("File downloaded successfully")
+    else:
+        print("File not found")
+
+    with zipfile.ZipFile(f"./data/{uuid}.zip", 'r') as zip_ref:
+        zip_ref.extractall(f"./data/{uuid}")
+    
+
+def make_response(uuid, numbers, success, output=None):
     try:
         return json.dumps({
             "reconstruction_id": uuid,
@@ -13,27 +31,32 @@ def make_response(uuid, numbers, success):
             "tracks": numbers[3],
             "estimated_tracks": numbers[4],
             "status": "finished",
+            "vertices": numbers[12] if len(numbers) > 10 else 0,
+            "faces": numbers[13] if len(numbers) > 10 else 0,
+            "output": output or "",
         })
     except BaseException:
-        return json.dumps({"status": "finished", "success": success})
+        return json.dumps({"status": "finished", "success": success, "output": output or ""})
 
 
 def init_reconstruction_task(uuid):
+    download_files(uuid)
     command = f"cd build/; ./reconstruction_cli init ../data/{uuid}/images/ ../data/{uuid}/camera_settings.txt ../data/{uuid}"
     output = subprocess.run(command, capture_output=True, shell=True).stdout.decode()
     numbers = re.findall("[-+]?(?:\d*\.\d+|\d+)", output)
     print(output)
     if len(numbers) > 1: 
         numbers.pop(1)
-    return make_response(uuid, list(map(float, numbers)), "Initialization successful" in output)
+    return make_response(uuid, list(map(float, numbers)), "Initialization successful" in output, output)
     
 
 def extend_reconstruction_task(uuid, number_of_images):
+    download_files(uuid)
     command = f"cd build/; ./reconstruction_cli extend ../data/{uuid}/images/ ../data/{uuid}/camera_settings.txt ../data/{uuid} {number_of_images - 1}"
     output = subprocess.run(command, capture_output=True, shell=True).stdout.decode()
     print(output)
     numbers = re.findall("[-+]?(?:\d*\.\d+|\d+)", output)
-    return make_response(uuid, list(map(float, numbers)), "Extend successful" in output)
+    return make_response(uuid, list(map(float, numbers)), "Extend successful" in output, output)
 
 def reconstruct_mesh_task(uuid):
     command = f"cd build/; ./reconstruction_cli reconstruct_mesh ../data/{uuid}/images/ ../data/{uuid}/camera_settings.txt ../data/{uuid}/"
@@ -44,6 +67,7 @@ def reconstruct_mesh_task(uuid):
     })
 
 def texture_task(uuid):
+    download_files(uuid)
     command = f"cd build/; ./reconstruction_cli texture ../data/{uuid}/images/ ../data/{uuid}/camera_settings.txt ../data/{uuid}/"
     output = subprocess.run(command, capture_output=True, shell=True).stdout.decode()
     return json.dumps({
@@ -53,6 +77,7 @@ def texture_task(uuid):
 
 
 def generate_ply_task(uuid):
+    download_files(uuid)
     command = f"cd build/; ./reconstruction_cli download ply ../data/{uuid}/images/ ../data/{uuid}/camera_settings.txt ../data/{uuid}/"
     output = subprocess.run(command, capture_output=True, shell=True).stdout.decode()
     return {
@@ -62,6 +87,7 @@ def generate_ply_task(uuid):
 
 
 def generate_ptam_task(uuid):
+    download_files(uuid)
     command = f"cd build/; ./reconstruction_cli ptam ../data/{uuid}/images/ ../data/{uuid}/camera_settings.txt ../data/{uuid}/"
     output = subprocess.run(command, capture_output=True, shell=True).stdout.decode()
     print(output)
--- worker.py
+++ worker.py
@@ -1,10 +1,13 @@
 import os
 
 import redis
+import requests
 from rq import Worker, Connection
 
 listen = ['default']
-conn = redis.from_url("redis://redis:6379/0")
+# conn = redis.from_url("redis://redis:6379/0")
+redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
     with Connection(conn):
